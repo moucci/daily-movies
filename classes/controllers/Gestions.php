1115973503ba53a798370156fa2da800eed6410d
Chosess <n.noelle552@gmail.com>
2023-06-30 14:36:31 +0200

    add gestions et Gestions

--- classes/controllers/Gestions.php
+++ classes/controllers/Gestions.php
@@ -3,12 +3,514 @@
 namespace classes\controllers;
 
 use classes\Core;
-use classes\Routes ; 
+use classes\Routes;
+use PDO;
+use PDOException;
 
 class Gestions extends MainController
 {
     public function __construct()
     {
-        MainController::render("gestions") ;
+
+        // Core::var_dump_pre(Routes::getParams()) ;
+        Routes::getParams();
+        MainController::render("gestions");
+    }
+
+
+    /**
+     * $db objest 
+     * user_id string -> l'id user
+     * $title string  -> le titre
+     * $content string -> le contenu
+     * $image string -> le nom de l'image
+     */
+    public static function addarticle(object $db, int $user_id, string $title, string $content, string $image)
+    {
+        $query = "INSERT INTO `articles` (user_id, title, content, image, slug) VALUES (:user_id, :title, :content, :image, :slug)";
+
+        $req = $db->prepare($query);
+
+
+        $req->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $req->bindParam(':title', $title, PDO::PARAM_STR);
+        $req->bindParam(':content', $content, PDO::PARAM_STR);
+        $req->bindParam(':image', $image, PDO::PARAM_STR);
+
+        $slug = Gestions::slugify($title);
+        $req->bindParam(':slug', $slug, PDO::PARAM_STR);
+
+        try {
+            $req->execute();
+            return [
+                'process' => true,
+                'message' => 'L\'article a bien été créé.'
+            ];
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
+
+        $req->closeCursor();
+    }
+
+
+    /**
+     * création d'une image 16:9
+     */
+    public static function addimagefull()
+    {
+
+        $image = $_FILES['image'];
+
+
+
+        // on vérifie l'extension et le type Mime
+        $allowed = [
+            'jpg' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'png' => 'image/png',
+        ];
+
+        $filename = $image['name'];
+        $filetype = $image['type'];
+        $filesize = $image['size'];
+
+
+
+
+
+        $extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+        if (!array_key_exists($extension, $allowed) || !in_array($filetype, $allowed)) {
+            //$erreurs['image'] = 'Format de fichier incorrect.';
+            return [
+                "process" => false,
+
+                "message_erreur" => "Format de fichier incorrect.",
+            ];
+        }
+
+        //on limite à 1 Mio
+        if ($filesize > 1024 * 1024) {
+        }
+
+
+
+        $newfilename = $image['tmp_name'];
+        //on génère un nom unique
+        $newname = md5(uniqid());
+
+        $newname2 = "$newname.$extension";
+
+        chmod($newfilename, 0644);
+
+        $dimension = getimagesize($newfilename);
+
+        $largeur = $dimension[0];
+        $hauteur = $dimension[1];
+
+
+        switch ($dimension['mime']) {
+            case 'image/png':
+                $imagesource = imagecreatefrompng($newfilename);
+                break;
+
+            case 'image/jpeg':
+                $imagesource = imagecreatefromjpeg($newfilename);
+                break;
+
+            default:
+                return [
+                    "process" => false,
+
+                    "message_erreur" => "un truc qui va pas",
+                ];
+                break;
+        }
+
+
+        $fullimage = imagecreatetruecolor($largeur, intval(9 * 100 / 16 * $largeur / 100));
+        $taille = intval(((100 - (($hauteur * 100) / $largeur)) / 2) * $largeur / 100);
+
+        imagecopyresampled(
+            $fullimage,
+            $imagesource,
+            0,
+            0,
+            0,
+            $taille,
+            $largeur,
+            $hauteur,
+            $largeur,
+            $hauteur,
+        );
+
+
+        switch ($dimension['mime']) {
+            case 'image/png':
+                imagepng($fullimage, "./public/assets/images/full/$newname.$extension");
+                break;
+
+            case 'image/jpeg':
+                imagejpeg($fullimage, "./public/assets/images/full/$newname.$extension");
+
+                break;
+        }
+
+        imagedestroy($imagesource);
+        imagedestroy($fullimage);
+
+
+        return [
+            "process" => true,
+
+            "name_img" => $newname2,
+        ];
+    }
+
+    /**
+     * création d'une image carré
+     * $name le nom généré avec unique id dans la fonction addimagefull
+     */
+    public static function addimagesquare($name)
+    {
+
+        $image = $_FILES['image'];
+
+
+
+        // on vérifie l'extension et le type Mime
+        $allowed = [
+            'jpg' => 'image/jpeg',
+            'jpeg' => 'image/jpeg',
+            'png' => 'image/png',
+        ];
+
+        $filename = $image['name'];
+        $filetype = $image['type'];
+        $filesize = $image['size'];
+
+
+
+
+
+        $extension = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
+
+        if (!array_key_exists($extension, $allowed) || !in_array($filetype, $allowed)) {
+            //$erreurs['image'] = 'Format de fichier incorrect.';
+            return [
+                "process" => false,
+
+                "message_erreur" => "Format de fichier incorrect.",
+            ];
+        }
+
+        //on limite à 1 Mio
+        if ($filesize > 1024 * 1024) {
+        }
+
+
+
+
+        $newfilename = $image['tmp_name'];
+
+
+
+        chmod($newfilename, 0644);
+
+        $dimension = getimagesize($newfilename);
+
+        $largeur = $dimension[0];
+        $hauteur = $dimension[1];
+
+
+        switch ($dimension['mime']) {
+            case 'image/png':
+                $imagesource = imagecreatefrompng($newfilename);
+                break;
+
+            case 'image/jpeg':
+                $imagesource = imagecreatefromjpeg($newfilename);
+                break;
+
+            default:
+                return [
+                    "process" => false,
+
+                    "message_erreur" => "un truc qui va pas",
+                ];
+                break;
+        }
+
+        if ($largeur <= $hauteur) {
+            $nouvelleimage = imagecreatetruecolor($largeur, $largeur);
+            $taille = intval(((100 - (($largeur * 100) / $hauteur)) / 2) * $hauteur / 100);
+
+            imagecopyresampled(
+                $nouvelleimage,
+                $imagesource,
+                0,
+                0,
+                0,
+                $taille,
+                $largeur,
+                $hauteur,
+                $largeur,
+                $hauteur,
+            );
+        }
+
+        if ($largeur > $hauteur) {
+            $nouvelleimage = imagecreatetruecolor($hauteur, $hauteur);
+            $taille = intval(((100 - (($hauteur * 100) / $largeur)) / 2) * $largeur / 100);
+
+            imagecopyresampled(
+                $nouvelleimage,
+                $imagesource,
+                0,
+                0,
+                $taille,
+                0,
+                $largeur,
+                $hauteur,
+                $largeur,
+                $hauteur,
+            );
+        }
+
+
+        switch ($dimension['mime']) {
+            case 'image/png':
+                imagepng($nouvelleimage, "./public/assets/images/square/$name");
+                break;
+
+            case 'image/jpeg':
+                imagejpeg($nouvelleimage, "./public/assets/images/square/$name");
+
+                break;
+        }
+
+        imagedestroy($imagesource);
+        imagedestroy($nouvelleimage);
+
+
+        return [
+            "process" => true,
+
+            "name_img" => $name,
+        ];
+    }
+
+
+    /**
+     * $db object 
+     * $user_id string -> l'id user
+     * $title string  -> le titre
+     * $content string -> le contenu
+     * $image string -> le nom de l'image
+     * $idcategorie int -> l'id de la catégorie
+     */
+    public static function addcategorie(object $db, int $user_id, string $title, string $content, string $image, int $idcategorie)
+    {
+
+        //on récupère l'id de l'article
+        $query = "SELECT * FROM `articles` WHERE `user_id` = :user_id AND `title` = :title AND `content` = :content AND `image` = :image";
+
+        $req = $db->prepare($query);
+
+
+        $req->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $req->bindParam(':title', $title, PDO::PARAM_STR);
+        $req->bindParam(':content', $content, PDO::PARAM_STR);
+        $req->bindParam(':image', $image, PDO::PARAM_STR);
+
+        try {
+            $req->execute();
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
+
+        $articleid = $req->fetch(PDO::FETCH_ASSOC);
+
+        //l'ajout dans article_categories
+        $query = "INSERT INTO `article_categories` (article_id, categorie_id) VALUES (:articleid, :idcategorie)";
+
+        $req = $db->prepare($query);
+
+
+        $req->bindParam(':articleid', $articleid['id'], PDO::PARAM_INT);
+        $req->bindParam(':idcategorie', $idcategorie, PDO::PARAM_INT);
+
+        try {
+            $req->execute();
+            return [
+                'process' => true,
+            ];
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
+
+
+        $req->closeCursor();
+    }
+
+
+
+    /**
+     * $db object 
+     * $name le nom de la catégorie que l'on veut créer
+     */
+    public static function createcategorie(object $db, string $name)
+    {
+
+        $query = "INSERT INTO `categories` (name, slug) VALUES (:name, :slug)";
+
+        $req = $db->prepare($query);
+
+        $req->bindParam(':name', $name, PDO::PARAM_STR);
+
+        $slug = Gestions::slugify($name);
+        $req->bindParam(':slug', $slug, PDO::PARAM_STR);
+
+        try {
+            $req->execute();
+            return [
+                'process' => true,
+                'message' => 'La catégorie a bien été ajoutée.',
+            ];
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
+
+        $req->closeCursor();
+    }
+
+
+    public static function slugify($text, string $divider = '-')
+    {
+        // replace non letter or digits by divider
+        $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+
+        // transliterate
+        $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+        // remove unwanted characters
+        $text = preg_replace('~[^-\w]+~', '', $text);
+
+        // trim
+        $text = trim($text, $divider);
+
+        // remove duplicate divider
+        $text = preg_replace('~-+~', $divider, $text);
+
+        // lowercase
+        $text = strtolower($text);
+
+        if (empty($text)) {
+            return 'n-a';
+        }
+
+        return $text;
+    }
+
+    /**
+     * $db -> object
+     * $img -> la nouvelle image
+     * $idarticle -> l'id de l'article 
+     */
+    public static function modifimg (object $db, string $img, int $idarticle)
+    {
+        $query = "UPDATE `articles` SET `image` = :img WHERE `id` = :id";
+
+        $req = $db->prepare($query);
+
+        $req->bindParam(':img', $img, PDO::PARAM_STR);
+        $req->bindParam(':id', $idarticle, PDO::PARAM_INT);
+
+
+        try {
+            $req->execute();
+            return [
+                'process' => true,
+                'message' => 'L\'image a bien été modifiée.',
+            ];
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
+    }
+
+
+    /**
+     * $db -> object
+     * $modiftitle -> le nouveau titre
+     * $idarticle -> l'id de l'article 
+     */
+    public static function modiftitle (object $db, string $modiftitle, int $idarticle)
+    {
+        $query = "UPDATE `articles` SET `title` = :title WHERE `id` = :id";
+
+        $req = $db->prepare($query);
+
+        $req->bindParam(':title', $modiftitle, PDO::PARAM_STR);
+        $req->bindParam(':id', $idarticle, PDO::PARAM_INT);
+
+
+        try {
+            $req->execute();
+            return [
+                'process' => true,
+                'message' => 'Le titre a bien été modifié.',
+            ];
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
+    }
+
+
+    /**
+     * $db -> object
+     * $modifcontent -> le nouveau titre
+     * $idarticle -> l'id de l'article 
+     */
+    public static function modifcontent (object $db, string $modifcontent, int $idarticle)
+    {
+        $query = "UPDATE `articles` SET `content` = :content WHERE `id` = :id";
+
+        $req = $db->prepare($query);
+
+        $req->bindParam(':content', $modifcontent, PDO::PARAM_STR);
+        $req->bindParam(':id', $idarticle, PDO::PARAM_INT);
+
+
+        try {
+            $req->execute();
+            return [
+                'process' => true,
+                'message' => 'Le contenu a bien été modifié.',
+            ];
+        } catch (PDOException $error) {
+            return [
+                'process' => false,
+                'message' => 'Une erreur est survenue, veuillez réessayer ultérieurement.',
+            ];
+        }
     }
 }
--- views/gestions.php
+++ views/gestions.php
@@ -1,27 +1,28 @@
 <?php
 
-require_once 'addarticle.php';
-require_once 'addimage.php';
-require_once 'addcategorie.php';
-
 use classes\Db;
+use classes\controllers\Gestions;
+use classes\Routes;
+use classes\controllers\Articles;
+use classes\controllers\Categories;
 
 $db = Db::getDb();
 
-$query = "SELECT * FROM `articles`";
+$categories = Categories::getAll();
 
-$req = $db->query($query);
+$articles = Articles::getAll();
+$articles = $articles['articles'];
 
-$articles = $req->fetchAll(PDO::FETCH_ASSOC);
+if (isset(Routes::getParams()[2])) {
+    $edit = Articles::getById(Routes::getParams()[2]);
+    $edit = $edit['article'];
+    // echo '<pre>';
+    // var_dump($edit);
+    // echo '</pre>';
+}
 
-//on récupère les catégories
-$query = "SELECT * FROM `categories`";
 
-$req = $db->query($query);
 
-$categories = $req->fetchAll(PDO::FETCH_ASSOC);
-
-//ajout d'un article
 $erreurs = [];
 
 
@@ -39,15 +40,8 @@ if (!empty($_POST)) :
 
 
 
-    //on traite l'image
 
-    if (isset($_FILES['image']) && $_FILES['image']['error'] === 0) {
-        $image = $_FILES['image'];
 
-        addimage($image);
-    } else {
-        $erreurs['image'] = 'Le champ image est obligatoire.';
-    }
 
 
     // on vérifie si la catégorie est vide
@@ -57,38 +51,182 @@ if (!empty($_POST)) :
     }
 
 
-    if (isset($_POST['title']) && empty($erreurs)) :
 
-        //à effacer
-        $_POST['user_id'] = 2;
-        //à effacer
+    //on traite l'image
+
+    if (isset($_FILES['image']) && $_FILES['image']['error'] === 0) {
+        $image = $_FILES['image'];
+
+        if (empty($erreurs)) {
+            $reponseimage = Gestions::addimagefull();
+            Gestions::addimagesquare($reponseimage["name_img"]);
+        }
+    } else {
+        $reponseimage['message_erreur'] = 'Le champ image est obligatoire.';
+    }
 
 
-        $user_id = $_POST['user_id'];
+
+    if (isset($_POST['title']) && empty($erreurs) && $reponseimage['process'] == true) :
+
+
+        $user_id = $_SESSION['id'];
         $title = $_POST['title'];
         $content = $_POST['content'];
-        $nomimage = $_SESSION['nomimage'];
+        $nomimage =  $reponseimage["name_img"];
 
 
-        addarticle($db, $user_id, $title, $content, $nomimage);
+        $messagearticle = Gestions::addarticle($db, $user_id, $title, $content, $nomimage);
+
 
         foreach ($_POST['categories'] as $categorie) {
-            addcategorie($db, $user_id, $title, $content, $nomimage, intval($categorie));
+            $messagecategorie = Gestions::addcategorie($db, $user_id, $title, $content, $nomimage, intval($categorie));
+
+            if ($messagecategorie['process'] == false && file_exists((__DIR__ . '../../public/assets/images/full/' . $nomimage))) {
+                unlink(__DIR__ . '../../public/assets/images/full/' . $nomimage);
+            }
+
+            if ($messagecategorie['process'] == false && file_exists((__DIR__ . '../../public/assets/images/square/' . $nomimage))) {
+                unlink(__DIR__ . '../../public/assets/images/square/' . $nomimage);
+            }
         }
 
 
+        if ($messagearticle['process'] == false && file_exists((__DIR__ . '../../public/assets/images/full/' . $nomimage))) {
+            unlink(__DIR__ . '../../public/assets/images/full/' . $nomimage);
+        }
+
+        if ($messagearticle['process'] == false && file_exists((__DIR__ . '../../public/assets/images/square/' . $nomimage))) {
+            unlink(__DIR__ . '../../public/assets/images/square/' . $nomimage);
+        }
+
 
     endif;
+
+
+    if (empty($_POST['addcat'])) {
+        $erreurs['addcat'] = 'Le champ est obligatoire';
+    }
+
+    if (!empty($_POST['addcat'])) {
+        $repcat = Gestions::createcategorie($db, $_POST['addcat']);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // la modif d'article
+
+
+    // //l'image
+    // if (!empty($_FILES['image']['name'])) {
+    //     $modifimg = Gestions::addimagefull();
+    //     Gestions::addimagesquare($modifimg["name_img"]);
+
+    //     if ($modifimg["process"] == true) {
+
+    //         $repmodifimg = Gestions::modifimg($db, $modifimg["name_img"], Routes::getParams()[2]);
+    //         var_dump($repmodifimg);
+    //     }
+    // }
+
+
+    // //le titre
+    // if (!empty($_POST['modiftitle']) &&  $_POST['modiftitle'] != $edit->title) {
+    //     $modiftitle = Gestions::modiftitle($db, $_POST['modiftitle'], Routes::getParams()[2]);
+    // }
+
+
+    // //le contenu
+    // if (!empty($_POST['modifcontent']) &&  $_POST['modifcontent'] != $edit->content) {
+    //     $modifcontent = Gestions::modifcontent($db, $_POST['modifcontent'], Routes::getParams()[2]);
+    // }
+
+
+    //les catégories
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //on rerécupère les infos de l'article
+    if (isset(Routes::getParams()[2])) {
+        $edit = Articles::getById(Routes::getParams()[2]);
+        $edit = $edit['article'];
+    }
+
 endif;
 
 ?>
 
 
 <section>
-    <?php if (isset($_GET['new'])) : ?>
+
+
+    <?php if (isset(Routes::getParams()[1]) && Routes::getParams()[1] == 'new') : ?>
 
         <!-- formulaire de création d'un article -->
-        <form class="border rounded p-2 position-relative m-auto mt-5" style="max-width: 80vw;" action="gestions.php" method="POST" enctype="multipart/form-data">
+        <form class="border rounded p-2 position-relative m-auto mt-5" style="max-width: 80vw;" method="POST" enctype="multipart/form-data">
 
             <div class="d-flex flex-column">
 
@@ -118,17 +256,20 @@ endif;
 
                         <?php foreach ($categories as $categorie) : ?>
 
-                            <option value="<?= $categorie['id'] ?>"><?= $categorie['name'] ?></option>
+                            <option value="<?= $categorie->id ?>"><?= $categorie->name ?></option>
 
                         <?php endforeach; ?>
 
                     </select>
+
+                    <a href="categorie">Ajouter une catégorie</a>
+
                 </div>
 
 
                 <div>
-                    <?php if (isset($erreurs['image'])) : ?>
-                        <span class="text-danger"><?= $erreurs['image'] ?></span>
+                    <?php if (isset($reponseimage['message_erreur'])) : ?>
+                        <span class="text-danger"><?= $reponseimage['message_erreur'] ?></span>
                     <?php endif; ?>
                     <!-- l'image de l'article -->
                     <input type="file" name="image" id="image" class="mt-3">
@@ -152,10 +293,142 @@ endif;
 
         </form>
 
+        <a href="/gestions">Revenir à la page gestion</a>
+
+
+
+
+
+
+
+
+    <?php elseif (isset(Routes::getParams()[1]) && Routes::getParams()[1] == 'categorie') : ?>
+
+        <form class="border rounded p-2 position-relative m-auto mt-5 d-flex flex-column text-center" style="max-width: 30vw;" method="POST">
+
+            <?php if (isset($erreurs['addcat'])) : ?>
+                <span class="text-danger"><?= $erreurs['addcat'] ?></span>
+            <?php endif; ?>
+
+            <input type="text" placeholder="Nom de la catégorie" name="addcat" class="mt-3 mb-3 text-center">
+
+            <?php if (isset($repcat['message'])) : ?>
+                <span class="text-danger"><?= $repcat['message'] ?></span>
+            <?php endif; ?>
+
+            <button>Créer la catégorie</button>
+
+        </form>
+
+
+        <a href="new" class="m-auto mt-5">Revenir à la création d'article</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <?php elseif (isset(Routes::getParams()[1]) && Routes::getParams()[1] == 'edit' && !empty(Routes::getParams()[2])) : ?>
+
+
+        <form class="border rounded p-2 position-relative m-auto mt-5" style="max-width: 60vw;" method="POST" enctype="multipart/form-data">
+
+            <div class="d-flex flex-column">
+
+                <img src="../../public/assets/images/full/<?= $edit->image ?>" alt="" class="card-img-top">
+                <!-- l'image -->
+                <div>
+                    <label for="image">Changer l'image</label>
+                    <input type="file" name="image" id="image" value="<?= $edit->image ?>">
+                </div>
+
+                <!-- le titre -->
+                <input type="text" name="modiftitle" value="<?= $edit->title ?>" class="mt-2 rounded" style="max-width: 500px;">
+
+                <!-- le contenu -->
+                <textarea type="text" name="modifcontent" class="mt-2 text-break overflow-visible rounded" style="height: 200px;"><?= $edit->content ?></textarea>
+
+                <!-- les catégories -->
+                <div>
+                    <label for="categories">Modifier les catégories (pour choisir plusieurs catégories maintenez 'ctrl' au moment du click)</label>
+
+                    <select name="modifcategories[]" id="categories" class="text-center" multiple size="2">
+
+                        <?php foreach ($categories as $categorie) : ?>
+
+                            <option value="<?= $categorie->id ?>,<?= $categorie->name ?>" <?php if (!empty($edit->cats)) {
+                                                                                                foreach ($edit->cats as $cat) {
+                                                                                                    if ($categorie->name == $cat) {
+                                                                                                        echo 'selected';
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                            ?>>
+                                <?= $categorie->name ?></option>
+
+                        <?php endforeach; ?>
+
+                    </select>
+                </div>
+
+                <button>Modifier l'article</button>
+            </div>
+        </form>
+        <?php
+        ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
     <?php else : ?>
 
         <button class="mt-2 text-center border border-secondary rounded-pill">
-            <a href="gestions?new" class="mb-0 p-2">Créer un article
+            <a href="gestions/new" class="mb-0 p-2">Créer un article
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                     <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
@@ -172,33 +445,22 @@ endif;
             ?>
 
                 <article class="card mb-5 ms-5" style="width: 18rem;">
-                    <a href="article.php?id=<?= $article['id'] ?>"><img src="../public/assets/images/full/<?= $article['image'] ?>" alt="" class="card-img-top"></a>
+                    <img src="../public/assets/images/full/<?= $article->image ?>" alt="" class="card-img-top">
                     <div class="card-body">
-                        <h3 class="card-title fst-italic text-capitalize"><a href="article.php?id=<?= $article['id'] ?>"><?= $article['title'] . ' :' ?></a></h3>
+                        <h3 class="card-title fst-italic text-capitalize"><?= $article->title . ' :' ?></h3>
                         <p class="card-text text-wrap">
-                            <?= substr($article['content'], 0, 200) . '...'; ?>
+                            <?= substr($article->content, 0, 200) . '...'; ?>
                         </p>
                     </div>
                     <div class="d-flex justify-content-evenly align-items-baseline">
                         <p class="fw-bold">
                             <?php
-
-                            $user_id = $article['user_id'];
-
-                            $query = "SELECT * FROM `users` WHERE `id` = $user_id";
-
-
-                            $req = $db->query($query);
-
-
-                            $user = $req->fetch(PDO::FETCH_ASSOC);
-
-                            echo $user['nom'] . ' ' . $user['prenom'];
+                            echo $article->nom . ' ' . $article->prenom;
 
                             ?>
                         </p>
 
-                        <a href="gestion.php?edit=<?= $article['id'] ?>" class="btn btn-primary">Editer</a>
+                        <a href="gestions/edit/<?= $article->id ?>" class="btn btn-primary">Editer</a>
 
 
                     </div>
